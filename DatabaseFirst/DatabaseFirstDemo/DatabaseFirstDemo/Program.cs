1b7019fea0631086e087d9b83a5b61a72afccd19
Petar Kobakov <pkobakov@gmail.com>
2023-01-21 12:43:59 +0200

    Database First Workflow

---
+++ DatabaseFirst/DatabaseFirstDemo/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Posts()
+            { 
+             PostID= 1,
+             Title="My FirstDatabase Demo",
+             Body="Body",
+             DatePublishded= DateTime.Now,
+            
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();  
+
+        }
+    }
+}
