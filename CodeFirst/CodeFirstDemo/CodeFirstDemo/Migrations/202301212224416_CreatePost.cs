0cbfda082aa6e51f0c2a6be358eb4681dd921474
Petar Kobakov <pkobakov@gmail.com>
2023-01-22 00:29:23 +0200

    Code First Workflow

---
+++ CodeFirst/CodeFirstDemo/CodeFirstDemo/Migrations/202301212224416_CreatePost.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeFirstDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Body = c.String(),
+                        DatePublished = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ CodeFirst/CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    public class Post 
+    { 
+      public int Id { get; set; }   
+      public string Title { get; set; }   
+      public string Body { get; set; }
+      public DateTime DatePublished { get; set; }
+    }
+
+    public class BlogContext: DbContext
+    { 
+      public DbSet<Post> Posts { get; set; }
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
